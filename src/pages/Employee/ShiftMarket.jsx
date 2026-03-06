ed860c8b1cf501392d21901925d91c86b8de9a20
NguyenTruongNT <nguyentruong100105@gmail.com>
2026-03-06 12:25:58 +0700

    UD.2: Đổi ca

--- src/pages/Employee/ShiftMarket.jsx
+++ src/pages/Employee/ShiftMarket.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Construction, ArrowLeft } from "lucide-react";
-import { useNavigate } from "react-router-dom";
 
 const ShiftMarket = () => {
-  const navigate = useNavigate();
 
   return (
     <div className="min-h-screen bg-slate-50 flex flex-col items-center justify-center font-sans">
